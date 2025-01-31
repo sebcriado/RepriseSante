87f8b4a1a066ebaff969ca7a5ab4dc60551c754b
sebcriado <contact.sebastiencriado@gmail.com>
2025-01-31 18:49:12 +0100

    add test for redirect to login and register page

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,9 +1,11 @@
 import { test, expect } from '@playwright/test';
 
 test.describe("Home Page", () => {
-    test("Should always have the elements and metadata", async({page}) => {
-        await page.goto("http://localhost:5173");
+    test.beforeEach(async ({page}) => {
+        await page.goto("http://localhost:5173")
+    });
 
+    test("Should always have the elements and metadata", async({page}) => {
         await expect(page).toHaveTitle("RepriseSanté");
 
         await expect(
@@ -23,5 +25,21 @@ test.describe("Home Page", () => {
                 name: "Inscription"
             })
         ).toBeVisible();
+    })
+
+    test("Should redirect to login page on click", async ({page}) => {
+        await page.getByRole("link", {
+            name: "Connexion"
+        }).click();
+
+        await expect(page).toHaveURL("http://localhost:5173/login");
+    })
+
+    test("Should redirect to register page on click", async ({page}) => {
+        await page.getByRole("link", {
+            name: "Inscription"
+        }).click();
+
+        await expect(page).toHaveURL("http://localhost:5173/register");
     })
 })
